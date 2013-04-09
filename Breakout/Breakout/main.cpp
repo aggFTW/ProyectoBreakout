d5c0fb381c1b5483a14b9c783940bc31b577bdf6
Alejandro Guerrero <alexgg@Alejandros-MacBook-Pro-3.local>
2013-04-09 16:02:39 -0500

    restructured code: init, reshape

--- Breakout/Breakout/main.cpp
+++ Breakout/Breakout/main.cpp
@@ -5,7 +5,7 @@
 //  Created by Alejandro Guerrero on 1/31/13.
 //  Copyright (c) 2013 Alejandro Guerrero. All rights reserved.
 //
-
+//#include <windows.h>
 #ifdef __APPLE__
 #include <GLUT/glut.h>
 #else
@@ -29,7 +29,7 @@ double ballX1 = -0.05;
 double ballX2 = +0.05;
 double ballY1 = -0.05;
 double ballY2 = +0.05;
-double radius = 0.03;   
+double radius = 0.03;
 
 double posXBall = 0.0;
 double posYBall = 0.0;
@@ -48,89 +48,89 @@ int blockLevel[20][20] = {0};
 
 
 
-float colorsAmbient[24][3] = {  0.0215 ,  0.1745 ,  0.0215 ,
-    0.135 ,  0.2225 ,  0.1575 ,
-    0.05375 ,  0.05 ,  0.06625 ,
-    0.25 ,  0.20725 ,  0.20725 ,
-    0.1745 ,  0.01175 ,  0.01175 ,
-    0.1 ,  0.18725 ,  0.1745 ,
-    0.329412 ,  0.223529 ,  0.027451 ,
-    0.2125 ,  0.1275 ,  0.054 ,
-    0.25 ,  0.25 ,  0.25 ,
-    0.19125 ,  0.0735 ,  0.0225 ,
-    0.24725 ,  0.1995 ,  0.0745 ,
-    0.19225 ,  0.19225 ,  0.19225 ,
-    0.0 ,  0.0 ,  0.0 ,
-    0.0 ,  0.1 ,  0.06 ,
-    0.0 ,  0.0 ,  0.0 ,
-    0.0 ,  0.0 ,  0.0 ,
-    0.0 ,  0.0 ,  0.0 ,
-    0.0 ,  0.0 ,  0.0 ,
-    0.02 ,  0.02 ,  0.02 ,
-    0.0 ,  0.05 ,  0.05 ,
-    0.0 ,  0.05 ,  0.0 ,
-    0.05 ,  0.0 ,  0.0 ,
-    0.05 ,  0.05 ,  0.05 ,
-    0.05 ,  0.05 ,  0.0 ,
+float colorsAmbient[24][4] = {  0.0215 ,  0.1745 ,  0.0215 ,1,
+    0.135 ,  0.2225 ,  0.1575 ,1,
+    0.05375 ,  0.05 ,  0.06625 ,1,
+    0.25 ,  0.20725 ,  0.20725 ,1,
+    0.1745 ,  0.01175 ,  0.01175 ,1,
+    0.1 ,  0.18725 ,  0.1745 ,1,
+    0.329412 ,  0.223529 ,  0.027451 ,1,
+    0.2125 ,  0.1275 ,  0.054 ,1,
+    0.25 ,  0.25 ,  0.25 ,1,
+    0.19125 ,  0.0735 ,  0.0225 ,1,
+    0.24725 ,  0.1995 ,  0.0745 ,1,
+    0.19225 ,  0.19225 ,  0.19225 ,1,
+    0.0 ,  0.0 ,  0.0 ,1,
+    0.0 ,  0.1 ,  0.06 ,1,
+    0.0 ,  0.0 ,  0.0 ,1,
+    0.0 ,  0.0 ,  0.0 ,1,
+    0.0 ,  0.0 ,  0.0 ,1,
+    0.0 ,  0.0 ,  0.0 ,1,
+    0.02 ,  0.02 ,  0.02 ,1,
+    0.0 ,  0.05 ,  0.05 ,1,
+    0.0 ,  0.05 ,  0.0 ,1,
+    0.05 ,  0.0 ,  0.0 ,1,
+    0.05 ,  0.05 ,  0.05 ,1,
+    0.05 ,  0.05 ,  0.0 ,1
 };
 
-float colorsDiffuse[24][3] = {  0.07568 ,  0.61424 ,  0.07568 ,
-    0.54 ,  0.89 ,  0.63 ,
-    0.18275 ,  0.17 ,  0.22525 ,
-    1 ,  0.829 ,  0.829 ,
-    0.61424 ,  0.04136 ,  0.04136 ,
-    0.396 ,  0.74151 ,  0.69102 ,
-    0.780392 ,  0.568627 ,  0.113725 ,
-    0.714 ,  0.4284 ,  0.18144 ,
-    0.4 ,  0.4 ,  0.4 ,
-    0.7038 ,  0.27048 ,  0.0828 ,
-    0.75164 ,  0.60648 ,  0.22648 ,
-    0.50754 ,  0.50754 ,  0.50754 ,
-    0.01 ,  0.01 ,  0.01 ,
-    0.0 ,  0.50980392 ,  0.50980392 ,
-    0.1 ,  0.35 ,  0.1 ,
-    0.5 ,  0.0 ,  0.0 ,
-    0.55 ,  0.55 ,  0.55 ,
-    0.5 ,  0.5 ,  0.0 ,
-    0.01 ,  0.01 ,  0.01 ,
-    0.4 ,  0.5 ,  0.5 ,
-    0.4 ,  0.5 ,  0.4 ,
-    0.5 ,  0.4 ,  0.4 ,
-    0.5 ,  0.5 ,  0.5 ,
-    0.5 ,  0.5 ,  0.4 ,
+float colorsDiffuse[24][4] = {  0.07568 ,  0.61424 ,  0.07568 ,1,
+    0.54 ,  0.89 ,  0.63 ,1,
+    0.18275 ,  0.17 ,  0.22525 ,1,
+    1 ,  0.829 ,  0.829 ,1,
+    0.61424 ,  0.04136 ,  0.04136 ,1,
+    0.396 ,  0.74151 ,  0.69102 ,1,
+    0.780392 ,  0.568627 ,  0.113725 ,1,
+    0.714 ,  0.4284 ,  0.18144 ,1,
+    0.4 ,  0.4 ,  0.4 ,1,
+    0.7038 ,  0.27048 ,  0.0828 ,1,
+    0.75164 ,  0.60648 ,  0.22648 ,1,
+    0.50754 ,  0.50754 ,  0.50754 ,1,
+    0.01 ,  0.01 ,  0.01 ,1,
+    0.0 ,  0.50980392 ,  0.50980392 ,1,
+    0.1 ,  0.35 ,  0.1 ,1,
+    0.5 ,  0.0 ,  0.0 ,1,
+    0.55 ,  0.55 ,  0.55 ,1,
+    0.5 ,  0.5 ,  0.0 ,1,
+    0.01 ,  0.01 ,  0.01 ,1,
+    0.4 ,  0.5 ,  0.5 ,1,
+    0.4 ,  0.5 ,  0.4 ,1,
+    0.5 ,  0.4 ,  0.4 ,1,
+    0.5 ,  0.5 ,  0.5 ,1,
+    0.5 ,  0.5 ,  0.4 ,1
 };
 
-float colorsSpecular[24][3] ={  0.633 ,  0.727811 ,  0.633 ,
-    0.316228 ,  0.316228 ,  0.316228 ,
-    0.332741 ,  0.328634 ,  0.346435 ,
-    0.296648 ,  0.296648 ,  0.296648 ,
-    0.727811 ,  0.626959 ,  0.626959 ,
-    0.297254 ,  0.30829 ,  0.306678 ,
-    0.992157 ,  0.941176 ,  0.807843 ,
-    0.393548 ,  0.271906 ,  0.166721 ,
-    0.774597 ,  0.774597 ,  0.774597 ,
-    0.256777 ,  0.137622 ,  0.086014 ,
-    0.628281 ,  0.555802 ,  0.366065 ,
-    0.508273 ,  0.508273 ,  0.508273 ,
-    0.5 ,  0.5 ,  0.5 ,
-    0.50196078 ,  0.50196078 ,  0.50196078 ,
-    0.45 ,  0.55 ,  0.45 ,
-    0.7 ,  0.6 ,  0.6 ,
-    0.7 ,  0.7 ,  0.7 ,
-    0.6 ,  0.6 ,  0.5 ,
-    0.4 ,  0.4 ,  0.4 ,
-    0.04 ,  0.7 ,  0.7 ,
-    0.04 ,  0.7 ,  0.04 ,
-    0.7 ,  0.04 ,  0.04 ,
-    0.7 ,  0.7 ,  0.7 ,
-    0.7 ,  0.7 ,  0.04 ,
+float colorsSpecular[24][4] ={  0.633 ,  0.727811 ,  0.633 ,1,
+    0.316228 ,  0.316228 ,  0.316228 ,1,
+    0.332741 ,  0.328634 ,  0.346435 ,1,
+    0.296648 ,  0.296648 ,  0.296648 ,1,
+    0.727811 ,  0.626959 ,  0.626959 ,1,
+    0.297254 ,  0.30829 ,  0.306678 ,1,
+    0.992157 ,  0.941176 ,  0.807843 ,1,
+    0.393548 ,  0.271906 ,  0.166721 ,1,
+    0.774597 ,  0.774597 ,  0.774597 ,1,
+    0.256777 ,  0.137622 ,  0.086014 ,1,
+    0.628281 ,  0.555802 ,  0.366065 ,1,
+    0.508273 ,  0.508273 ,  0.508273 ,1,
+    0.5 ,  0.5 ,  0.5 ,1,
+    0.50196078 ,  0.50196078 ,  0.50196078 ,1,
+    0.45 ,  0.55 ,  0.45 ,1,
+    0.7 ,  0.6 ,  0.6 ,1,
+    0.7 ,  0.7 ,  0.7 ,1,
+    0.6 ,  0.6 ,  0.5 ,1,
+    0.4 ,  0.4 ,  0.4 ,1,
+    0.04 ,  0.7 ,  0.7 ,1,
+    0.04 ,  0.7 ,  0.04 ,1,
+    0.7 ,  0.04 ,  0.04 ,1,
+    0.7 ,  0.7 ,  0.7 ,1,
+    0.7 ,  0.7 ,  0.04 ,1
 };
 
 
 float mat_ambient[] = {0.7f, 0.7f, 0.7f, 1.0f};
 float mat_diffuse[] = {0.6f, 0.6f, 0.6f, 1.0f};
 float mat_specular[] = {1.0f, 1.0f, 1.0f, 1.0f};
-GLfloat mat_shininess[]= {50.0f};
+GLfloat mat_shininess[]= {100.0f};
 
 // 0  "emerald"
 // 1  "jade"
@@ -158,27 +158,11 @@ GLfloat mat_shininess[]= {50.0f};
 // 23 "yellow rubber"
 void changeMaterial(int i)
 {
-    mat_ambient[0] = colorsAmbient[i][0];
-    mat_ambient[1] = colorsAmbient[i][1];
-    mat_ambient[2] = colorsAmbient[i][2];
-    mat_diffuse[0] = colorsDiffuse[i][0];
-    mat_diffuse[1] = colorsDiffuse[i][1];
-    mat_diffuse[2] = colorsDiffuse[i][2];
-    mat_specular[0] = colorsSpecular[i][0];
-    mat_specular[1] = colorsSpecular[i][1];
-    mat_specular[2] = colorsSpecular[i][2];
-    
-    glMaterialfv(GL_FRONT,GL_AMBIENT,mat_ambient);
-    glMaterialfv(GL_FRONT,GL_DIFFUSE,mat_diffuse);
-    glMaterialfv(GL_FRONT,GL_SPECULAR,mat_specular);
+    glMaterialfv(GL_FRONT,GL_AMBIENT,colorsAmbient[i]);
+    glMaterialfv(GL_FRONT,GL_DIFFUSE,colorsDiffuse[i]);
+    glMaterialfv(GL_FRONT,GL_SPECULAR,colorsSpecular[i]);
     glMaterialfv(GL_FRONT,GL_SHININESS,mat_shininess);
 }
-
-
-
-
-
-
 
 void printBlockLevelMatrix()
 {
@@ -238,9 +222,6 @@ static void generateRandomBlockLevelSymmetrical(int numberRows)
     
     // printBlockLevelMatrix();
 }
-
-
-
 
 
 
@@ -326,6 +307,20 @@ static void drawBlocks()
     }
 }
 
+
+void reshape(int x, int y)
+{
+    // Camera
+    glViewport(0, 0, x, y);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    double winHt = 1.0;
+    glOrtho(-winHt, winHt, -winHt, winHt, -winHt, winHt);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(0, 0.1, 0.4, 0, 0, 0, 0, 1, 0);
+}
+
 static void display(void)
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -335,15 +330,7 @@ static void display(void)
     GLfloat light_position[] = {2.0f, 2.0f, 3.0f, 0.0f};
     glLightfv(GL_LIGHT0, GL_POSITION, light_position);
     glLightfv(GL_LIGHT0, GL_DIFFUSE, lightIntensity);
-
-    // Camera
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    double winHt = 1.0;
-    glOrtho(-winHt, winHt, -winHt, winHt, -winHt, winHt);
-    gluLookAt(0, 0.1, 0.4, 0, 0, 0, 0, 1, 0);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
+    
     
     // Draw left paddle
     changeMaterial(0);              //emerald
@@ -371,7 +358,7 @@ static void display(void)
 
 // return true if ball in between x range of pad
 bool checkBallCollisionWithPad()
-{        
+{
     // Check x positions
     if (ballX1 <= padX2 && ballX2 >= padX1 && ballY1 <= padY2 && ballY1 >= (padY1+padY2)/2) {
         return true;
@@ -383,7 +370,7 @@ bool checkBallCollisionWithPad()
 
 
 void updateBallPosition()
-{    
+{
     posXBall += (xDir * xChange);
     posYBall += (yDir * yChange);
     
@@ -403,7 +390,7 @@ void updateBallPosition()
 
 
 bool gameOver() {
-    state = 1;
+    
     
     return false;
 }
@@ -522,7 +509,12 @@ void myKeyboard(unsigned char theKey, int mouseX, int mouseY)
 
 void init()
 {
-    gluOrtho2D(-1, 1, -1, 1);
+    //gluOrtho2D(-1, 1, -1, 1);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glShadeModel(GL_SMOOTH);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_NORMALIZE);
     
     generateRandomBlockLevelSymmetrical(10);
 }
@@ -532,19 +524,16 @@ int main(int argc, char *argv[])
     glutInit(&argc, argv);
     glutInitWindowSize(screenWidth, screenHeight);
     glutInitWindowPosition(10,10);
-    glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE | GLUT_DEPTH);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
     
     glutCreateWindow("Breakout");
     
-    glEnable(GL_LIGHTING);
-    glEnable(GL_LIGHT0);
-    glShadeModel(GL_SMOOTH);
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_NORMALIZE);
     
     glutKeyboardFunc(myKeyboard);
     glutDisplayFunc(display);
     glutTimerFunc(timeRedraw, myTimer, 1);
+    glutReshapeFunc(reshape );
+    //srand ((unsigned) time(NULL));
     
     init();
     
@@ -552,3 +541,4 @@ int main(int argc, char *argv[])
     
     return EXIT_SUCCESS;
 }
+
